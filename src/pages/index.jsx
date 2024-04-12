f0ddd685862d4a7099d0fea43340a52368b4bdaa
Nathan Jones <nathan@jonesco.autos>
2024-04-12 13:58:43 -0500

    a

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -38,7 +38,6 @@ export default function Home() {
         <RacePrep/>
         <CTAC />
         <Finance />
-        <Contact />
 
         <Script type="text/javascript" src="https://jones-co.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/xtxyma/b/6/c95134bc67d3a521bb3f4331beb9b804/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=4152b955"></Script>
 
