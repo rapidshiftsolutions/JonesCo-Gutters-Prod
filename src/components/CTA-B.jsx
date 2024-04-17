f8bfe0b13f79d75e758d94aaf80207e2f75af993
Nathan Jones <nathan@jonesco.autos>
2024-04-16 20:02:46 -0500

    a

--- src/components/CTA-B.jsx
+++ src/components/CTA-B.jsx
@@ -34,10 +34,19 @@ export default function Example() {
       </div>
       <div className="relative mx-auto max-w-7xl py-24 sm:py-32 lg:px-8 lg:py-40">
         <div className="pl-6 pr-6 md:ml-auto md:w-2/3 md:pl-16 lg:w-1/2 lg:pl-24 lg:pr-0 xl:pl-32">
-          <h2 className="text-base font-semibold leading-7 text-JonesCo-Green-400">Award winning support</h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-5xl">Expert Care.</p>
+          <h2 className="text-base font-semibold leading-7 text-JonesCo-Green-400">
+            Award winning support
+          </h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-5xl">
+            Expert Care.
+          </p>
           <p className="mt-6 text-base leading-7 text-JonesCo-Blue-300">
-          At JonesCo, we’re not just about gutters; we’re about people. From the initial quote to final installation and maintenance, our dedicated team provides personalized support every step of the way. Our award-winning assistance ensures your gutter needs are met with expertise and care. For us, no question is too small, no problem too big.
+            At JonesCo, we’re not just about gutters; we’re about people. From
+            the initial estimate to final installation and maintenance, our
+            dedicated team provides personalized support every step of the way.
+            Our award-winning assistance ensures your gutter needs are met with
+            expertise and care. For us, no question is too small, no problem too
+            big.
           </p>
           <div className="mt-8">
             <a
