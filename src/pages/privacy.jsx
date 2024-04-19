e928354366faa59138959da78cb0dde3ce83136e
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-04-18 19:34:25 -0500

    a

--- src/pages/privacy.jsx
+++ src/pages/privacy.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>JonesCo Seamless Gutter Systems || Home</title>
+        <title>JonesCo Seamless Gutter Systems || Privacy Policy</title>
         <meta name="theme-color" content="#C5F5CA" />
         <meta name="robots" content="index, follow" />
         <meta
