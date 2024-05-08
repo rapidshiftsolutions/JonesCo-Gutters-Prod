c25c85c10b7039c78abf933dbbff3e3a7c7c2475
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-05-08 10:21:03 -0500

    a

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -29,11 +29,11 @@ export default function Example() {
       </div>
       <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
         <p className="text-sm leading-6 text-gray-900">
-          <strong className="font-semibold">MONTHLY DISCOUNT</strong>
+          <strong className="font-semibold">  <a href="https://clienthub.getjobber.com/booking/cfb34696-fc96-46da-87a4-b54410c83f3b">WEEKLY DISCOUNT </a></strong>
           <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
             <circle cx={1} cy={1} r={1} />
           </svg>
-          10% OFF FOR NEW CUSTOMERS
+          <a href="https://clienthub.getjobber.com/booking/cfb34696-fc96-46da-87a4-b54410c83f3b">10% FOR NEW CUSTOMERS</a>
         </p>
       </div>
       <div className="flex flex-1 justify-end">
