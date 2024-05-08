521558f0f023b3172bcaf91f9149870ba62694f2
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-05-08 09:50:58 -0500

    a

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -2,7 +2,7 @@ import { XMarkIcon } from '@heroicons/react/20/solid'
 
 export default function Example() {
   return (
-    <div className="relative isolate flex items-center gap-x-6 overflow-hidden px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+    <div className="bg-JonesCo-Blue-50 relative isolate flex items-center gap-x-6 overflow-hidden px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
       <div
         className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
         aria-hidden="true"
@@ -29,11 +29,11 @@ export default function Example() {
       </div>
       <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
         <p className="text-sm leading-6 text-gray-900">
-          <strong className="font-semibold">Regional Opening</strong>
+          <strong className="font-semibold">MONTHLY DISCOUNT</strong>
           <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
             <circle cx={1} cy={1} r={1} />
           </svg>
-          Now in Eastern TN
+          10% OFF FOR NEW CUSTOMERS
         </p>
       </div>
       <div className="flex flex-1 justify-end">
