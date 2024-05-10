29e7886c5187be163d7143ee4bbca7d3e7444671
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-05-10 15:47:03 -0500

    a

--- src/components/googleTag.jsx
+++ src/components/googleTag.jsx
@@ -1,10 +1,10 @@
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleTagManager } from '@next/third-parties/google'
  
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="AW-16543948897" />
       <body>{children}</body>
-      <GoogleAnalytics gaId="AW-16543948897" />
     </html>
   )
 }
