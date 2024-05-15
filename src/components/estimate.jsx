875a1ba02878b5975aadb00e7a5989be274a8553
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-05-15 14:29:27 -0500

    a

--- src/components/estimate.jsx
+++ src/components/estimate.jsx
@@ -1,39 +1,12 @@
-'use client';
-
-import Script from 'next/script';
-import { useEffect } from 'react';
-
-export default function JobberBookingForm() {
-  const CLIENTHUB_ID = '1b0129bf-9730-46bf-9d7c-a34501f74690'; 
-  const FORM_URL = `https://clienthub.getjobber.com/client_hubs/${CLIENTHUB_ID}/public/work_request/embedded_work_request_form`;
-
-  useEffect(() => {
-    const initJobber = () => {
-      if (typeof window !== 'undefined' && window.Jobber) {
-        window.Jobber.init({ clientHubId: CLIENTHUB_ID });
-      } else {
-        // If Jobber is not yet loaded, retry after a delay
-        setTimeout(initJobber, 500); 
-      }
-    };
-
-    initJobber(); // Call the function to initiate Jobber
-  }, []);
-
+export default function Example() {
   return (
-    <div className="flex flex-col justify-center py-6 sm:py-12 bg-JonesCo-Green-200 w-full">
-      <div id={CLIENTHUB_ID}></div> 
-
-      <Script
-        src="https://d3ey4dbjkt2f6s.cloudfront.net/assets/static_link/work_request_embed_snippet.js"
-        data-clienthub-id={CLIENTHUB_ID}
-        data-form-url={FORM_URL}
-        strategy="afterInteractive"
-      />
-      <Script 
-        src="https://d3ey4dbjkt2f6s.cloudfront.net/assets/external/work_request_embed.css"
-        strategy="afterInteractive"
-      />
-    </div>
-  );
+    <>
+      <div >
+        <div className="flex flex-col justify-center py-6 sm:py-12 bg-JonesCo-Green-200 w-full">
+          <div id="1b0129bf-9730-46bf-9d7c-a34501f74690"></div>
+          <link rel="stylesheet" href="https://d3ey4dbjkt2f6s.cloudfront.net/assets/external/work_request_embed.css" media="screen" />
+          <script src="https://d3ey4dbjkt2f6s.cloudfront.net/assets/static_link/work_request_embed_snippet.js" clienthub_id="1b0129bf-9730-46bf-9d7c-a34501f74690" form_url="https://clienthub.getjobber.com/client_hubs/1b0129bf-9730-46bf-9d7c-a34501f74690/public/work_request/embedded_work_request_form"></script>
+        </div></div>
+    </>
+  )
 }
