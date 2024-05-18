c7933e4294478a1a124b6c5998bcecb1ab2c51b0
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-05-18 09:53:35 -0500

    a

--- src/components/FreeEstimateForm.jsx
+++ src/components/FreeEstimateForm.jsx
@@ -90,6 +90,32 @@ export default function Example() {
                     />
                   </div>
                 </div>
+                <div>
+                  <label htmlFor="website" className="block text-sm font-semibold leading-6 text-JonesCo-Blue-900">
+                    Email
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="website"
+                      id="website"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-JonesCo-Blue-900 shadow-sm ring-1 ring-inset ring-JonesCo-Blue-300 placeholder:text-JonesCo-Blue-400 focus:ring-2 focus:ring-inset focus:ring-JonesCo-Green-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label htmlFor="website" className="block text-sm font-semibold leading-6 text-JonesCo-Blue-900">
+                    Phone Number
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="website"
+                      id="website"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-JonesCo-Blue-900 shadow-sm ring-1 ring-inset ring-JonesCo-Blue-300 placeholder:text-JonesCo-Blue-400 focus:ring-2 focus:ring-inset focus:ring-JonesCo-Green-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
                 <div className="sm:col-span-2">
                   <label htmlFor="message" className="block text-sm font-semibold leading-6 text-JonesCo-Blue-900">
                     In a few words, describe how we can help.
