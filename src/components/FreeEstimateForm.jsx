571f60848c67a381592661e7ed87211e3e8793ec
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-05-18 10:18:48 -0500

    a

--- src/components/FreeEstimateForm.jsx
+++ src/components/FreeEstimateForm.jsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Script from 'next/script'
 
 
 export default function Example() {
@@ -29,45 +29,9 @@ export default function Example() {
                 <rect width="100%" height="100%" strokeWidth={0} fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)" />
             </svg>
             <div className="mx-auto max-w-xl lg:max-w-4xl">
-                <h2 className="text-2xl sm:text-4xl font-bold tracking-tight text-JonesCo-Blue-900">Let’s talk about your project</h2>
-                <p className="mt-2 text-lg leading-8 text-JonesCo-Blue-600">
-                    We help companies and individuals solve their gutter problems.
-                </p>
-                <div className="mt-16 flex flex-col gap-16 sm:gap-y-20 lg:flex-row">
-                    <form name="contact" netlify>
-                        <p>
-                            <label>Name <input type="text" name="name" /></label>
-                        </p>
-                        <p>
-                            <label>Email <input type="email" name="email" /></label>
-                        </p>
-                        <p>
-                            <button type="submit">Send</button>
-                        </p>
-                    </form>
-                    <div className="lg:mt-6 lg:w-80 lg:flex-none">
-                        <img className="h-12 w-auto" src="/JC/JonesCo.webp" alt="" />
-                        <figure className="mt-10">
-                            <blockquote className="text-lg font-semibold leading-8 text-JonesCo-Blue-900">
-                                <p>
-                                    “Our mission is to make sure the job is done right, the first time, every time.
-                                    If you aren&apos;t happy, we&apos;ll make it right.”
-                                </p>
-                            </blockquote>
-                            <figcaption className="mt-10 flex gap-x-6">
-                                <img
-                                    src="/Stock/john.webp"
-                                    alt=""
-                                    className="h-12 w-12 flex-none rounded-full bg-JonesCo-Blue-50"
-                                />
-                                <div>
-                                    <div className="text-base font-semibold text-JonesCo-Blue-900">John Jones</div>
-                                    <div className="text-sm leading-6 text-JonesCo-Blue-600">CEO of JonesCo</div>
-                                </div>
-                            </figcaption>
-                        </figure>
-                    </div>
-                </div>
+                <div id="1b0129bf-9730-46bf-9d7c-a34501f74690"></div>
+                <link rel="stylesheet" href="https://d3ey4dbjkt2f6s.cloudfront.net/assets/external/work_request_embed.css" media="screen" />
+                <Script src="https://d3ey4dbjkt2f6s.cloudfront.net/assets/static_link/work_request_embed_snippet.js" clienthub_id="1b0129bf-9730-46bf-9d7c-a34501f74690" form_url="https://clienthub.getjobber.com/client_hubs/1b0129bf-9730-46bf-9d7c-a34501f74690/public/work_request/embedded_work_request_form" />
             </div>
         </div>
     )
