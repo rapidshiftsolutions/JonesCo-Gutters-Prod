8ffd5c5a294f1551a84e93340c572f476b374275
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-06-07 12:46:36 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,14 +15,14 @@ const Hero = () => {
   return (
     <div className="relative h-screen">
       <iframe
-        className="absolute top-0 left-0 w-full h-full object-cover"
+        className="absolute top-0 left-0 w-full h-full object-cover md:-mb-16"
         src="https://www.youtube.com/embed/CwsZA7ljSlk?autoplay=1&mute=1&loop=1&playlist=CwsZA7ljSlk"
         title="Background Video"
         frameBorder="0"
         allow="autoplay; loop; fullscreen"
       ></iframe>
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
-      <div className="relative z-10 flex items-center justify-center h-full  pt-16 md:pt-36 md:-mb-16">
+      <div className="relative z-10 flex items-center justify-center h-full  pt-16 md:pt-36 ">
         <zapier-interfaces-page-embed
           page-id="clx4ut3lm000hqp5wgrxt7v2f"
           no-background="true"
