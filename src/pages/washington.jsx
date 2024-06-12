36befc6dc8ebb179061342a6e105e3e9937e98dd
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-06-12 13:07:14 -0500

    Update washington.jsx

--- src/pages/washington.jsx
+++ src/pages/washington.jsx
@@ -1,1 +1,31 @@
 
+import Head from 'next/head'
+import Banner from '@/components/banner'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import CTAA from '@/components/CTA-B'
+import WashingtonCounty from '@/components/WashingtonCounty'
+import OurServices from '@/components/OurServices'
+import ServicesExtended from '@/components/OurServicesExt'
+
+export default function WashingtonCountyPage() {
+  return (
+    <>
+      <Head>
+        <title>JonesCo Seamless Gutter Systems || Washington County</title>
+        <meta name="theme-color" content="#C5F5CA" />
+        <meta
+          name="description"
+          content="Expert Installations for Lasting Home Protection in Washington County."
+        />
+      </Head>
+      <Banner />
+      <Header />
+      <main className="">
+        <WashingtonCounty />
+        <CTAA />
+      </main>
+      <Footer />
+    </>
+  )
+}
