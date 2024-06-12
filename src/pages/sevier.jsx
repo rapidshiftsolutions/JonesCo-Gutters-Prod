0e8f95c085d60c870c037ee91096bccb9a298164
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-06-12 13:06:57 -0500

    Update sevier.jsx

--- src/pages/sevier.jsx
+++ src/pages/sevier.jsx
@@ -1,1 +1,31 @@
+import Head from 'next/head'
+import Banner from '@/components/banner'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import CTAA from '@/components/CTA-B'
+import SevierCounty from '@/components/SevierCounty'
+import OurServices from '@/components/OurServices'
+import ServicesExtended from '@/components/OurServicesExt'
+
+export default function SevierCountyPage() {
+  return (
+    <>
+      <Head>
+        <title>JonesCo Seamless Gutter Systems || Sevier County</title>
+        <meta name="theme-color" content="#C5F5CA" />
+        <meta
+          name="description"
+          content="Expert Installations for Lasting Home Protection in Sevier County."
+        />
+      </Head>
+      <Banner />
+      <Header />
+      <main className="">
+        <SevierCounty />
+        <CTAA />
+      </main>
+      <Footer />
+    </>
+  )
+}
 
