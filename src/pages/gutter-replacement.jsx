817dd7a1ff3f1838e3694fbd07bb69f822616977
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-06-12 10:15:10 -0500

    Update gutter-replacement.jsx

--- src/pages/gutter-replacement.jsx
+++ src/pages/gutter-replacement.jsx
@@ -1,1 +1,29 @@
+import Head from 'next/head'
+import Banner from '@/components/banner'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import  CTAA  from '@/components/CTA-B'
+import OurServices from '@/components/OurServices'
+import ServicesExtended from '@/components/OurServicesExt'
 
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>JonesCo Seamless Gutter Systems || Services</title>
+        <meta name="theme-color" content="#C5F5CA" />
+        <meta
+          name="description"
+          content="Expert Installations for Lasting Home Protection."
+        />
+      </Head>
+      <Banner />
+      <Header />
+      <main className="">
+        <OurServices />
+        <CTAA />
+      </main>
+      <Footer />
+    </>
+  )
+}
