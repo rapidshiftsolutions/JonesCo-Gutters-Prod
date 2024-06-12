fb58a2c37deb1aad564059c4875158cb0547d6cd
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-06-12 13:07:41 -0500

    Update blount.jsx

--- src/pages/blount.jsx
+++ src/pages/blount.jsx
@@ -1,1 +1,31 @@
+import Head from 'next/head'
+import Banner from '@/components/banner'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import CTAA from '@/components/CTA-B'
+import BlountCounty from '@/components/BlountCounty'
+import OurServices from '@/components/OurServices'
+import ServicesExtended from '@/components/OurServicesExt'
+
+export default function BlountCountyPage() {
+  return (
+    <>
+      <Head>
+        <title>JonesCo Seamless Gutter Systems || Blount County</title>
+        <meta name="theme-color" content="#C5F5CA" />
+        <meta
+          name="description"
+          content="Expert Installations for Lasting Home Protection in Blount County."
+        />
+      </Head>
+      <Banner />
+      <Header />
+      <main className="">
+        <BlountCounty />
+        <CTAA />
+      </main>
+      <Footer />
+    </>
+  )
+}
 
