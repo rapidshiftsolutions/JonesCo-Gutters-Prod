755243921e8e0ede3d8e53ef3868ee8c5acdbbf8
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-06-12 13:06:44 -0500

    Update greene.jsx

--- src/pages/greene.jsx
+++ src/pages/greene.jsx
@@ -1,1 +1,31 @@
 
+import Head from 'next/head'
+import Banner from '@/components/banner'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import CTAA from '@/components/CTA-B'
+import GreeneCounty from '@/components/GreeneCounty'
+import OurServices from '@/components/OurServices'
+import ServicesExtended from '@/components/OurServicesExt'
+
+export default function GreeneCountyPage() {
+  return (
+    <>
+      <Head>
+        <title>JonesCo Seamless Gutter Systems || Greene County</title>
+        <meta name="theme-color" content="#C5F5CA" />
+        <meta
+          name="description"
+          content="Expert Installations for Lasting Home Protection in Greene County."
+        />
+      </Head>
+      <Banner />
+      <Header />
+      <main className="">
+        <GreeneCounty />
+        <CTAA />
+      </main>
+      <Footer />
+    </>
+  )
+}
