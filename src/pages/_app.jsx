1a7467bfe08243aa46eaa0424517a02c2ec22a91
Nathan Jones <nathan@jonesco.autos>
2024-07-01 17:30:47 +0000

    a

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,9 @@ import 'focus-visible'
 import '@/styles/tailwind.css'
 
 import GoogleTagManager from '../components/GoogleTagManager';
+import GoogleAnalytics from '@/components/GoogleAnalytics';
+
+
 
 function MyApp({ Component, pageProps }) {
   return (
