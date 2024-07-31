66afb60c1a96721eb66a4423211ad03311fa5e74
Nathan Jones <127130375+nathan-jonesco@users.noreply.github.com>
2024-07-30 19:07:59 -0500

    a

--- next.config.js
+++ next.config.js
@@ -16,10 +16,35 @@ const nextConfig = {
   },
   webpack: (config, { isServer }) => {
     // Modify the output.publicPath
-    config.output.publicPath = '/';
+    config.output.publicPath = '/public/';
 
     // Important: return the modified config
     return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/css',
+            condition: (path) => path.endsWith('.css'),
+          },
+          {
+            key: 'Content-Type',
+            value: 'application/javascript',
+            condition: (path) => path.endsWith('.js'),
+          },
+          {
+            key: 'Content-Type',
+            value: 'image/webp',
+            condition: (path) => path.endsWith('.webp'),
+          },
+          // Add other MIME type headers as needed
+        ],
+      },
+    ];
   },
 };
 
